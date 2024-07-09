e3989ee3594408ff807e2452367636e31c6d2eda
sametorbuk <orbuk60@gmail.com>
2024-07-09 13:23:36 +0300

    düzenleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
 import './App.css'
 
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min'
+import MainPageContent from './components/Main-page/MainPageContent'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
+         <Switch>
+        <Route path="/" exact>
+        <MainPageContent/>
+       </Route>
+        <Route path="/order">
+
+        
+       </Route>
+        </Switch>
+
+      
     </>
   )
 }
---
+++ src/components/Main-page/MainPageContent.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+
+export default function MainPageContent() {
+
+return (
+
+<>
+
+<div className='main-page-content' >
+      <p className='main-page-row1'>Teknolojik Yemekler</p>
+     <p className='main-page-row2' >KOD ACIKTIRIR</p> 
+     <p className='main-page-row3' >PİZZA DOYURUR</p>
+      <Link to="/order" ><button className='hungry-button' >ACIKTIM</button></Link>  
+      </div>
+</>
+)
+}
